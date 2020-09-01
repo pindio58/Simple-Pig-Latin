06c08d2db6a7d7d996695f0628575bdcb4f72d0e
pindio58 <pindio58@gmail.com>
2020-09-01 10:40:33 +0200

    first push after following PEP guidelines

--- SimplePigLatin.py
+++ SimplePigLatin.py
@@ -1,11 +1,18 @@
 import string
-puncs = string.punctuation
 
-def pig_it(text):
-    req_list=[]
-    for letter in text.split():
-        
-        if letter not in puncs: req_list.append(letter[1:]+letter[0]+'ay')
-        else: req_list.append(letter)
-    
-    return' '.join(letter for letter in req_list)
+signs = string.punctuation
+
+
+def pig_it (text):
+    req_list = [ ]
+    for letter in text.split ():
+
+        if letter not in signs:
+            req_list.append (letter[ 1: ] + letter[ 0 ] + 'ay')
+        else:
+            req_list.append (letter)
+
+    return ' '.join (letter for letter in req_list)
+
+
+print (pig_it ('Pig latin is cool'))
